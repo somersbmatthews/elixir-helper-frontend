46fd46637a89e8c97eaf958413488d9c48941123
Somers Matthews <somersbmatthews@gmail.com>
2019-10-28 18:29:04 -0500

    got codemirror to highlight elixir

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,7 +11,7 @@ module.exports = function(defaults) {
     // Add options here
   });
 
-  app.import('node_modules/codemirror/mode/elixir/elixir.js')
+ 
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
